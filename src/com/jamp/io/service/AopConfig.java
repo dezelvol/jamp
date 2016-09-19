4bc0fd319fd8ade127b0361f73502feb4045c35f
ivan <amputator2000@gmail.com>
2016-09-19 15:24:22 +0300

    Added aspect for logging / not working

---
+++ src/com/jamp/io/service/AopConfig.java
@@ -0,0 +1,10 @@
+package com.jamp.io.service;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+//@Configuration
+//@EnableAspectJAutoProxy
+public class AopConfig {
+
+}
