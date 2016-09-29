734e2e33aa76b0b0b8f63264381d10d287d9712c
ivan <amputator2000@gmail.com>
2016-09-29 15:00:28 +0300

    Updated with generated on start.spring.io

--- src/test/java/com/jamp/io/JampbootApplicationTests.java
+++ src/test/java/com/jamp/io/JampbootApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example;
+package com.jamp.io;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class DemoApplicationTests {
+public class JampbootApplicationTests {
 
 	@Test
 	public void contextLoads() {
